4c1435880415071375b2b8e2ad5683e6b77f154f
James Hobin <jhobin@ptc.com>
2020-12-16 12:40:00 -0500

    Fix checkout.sh paths

--- checkout.sh
+++ checkout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-cp "C:\\Program Files\\Azure Kinect SDK v1.3.0\\sdk\\windows-desktop\\amd64\\release\\bin\\*.dll" ./
-cp "C:\\Program Files\\Azure Kinect Body Tracking SDK\\sdk\\windows-desktop\\amd64\\release\\bin\\\\*" ./
-cp "C:\\Program Files\\Azure Kinect SDK v1.3.0\\sdk\\windows-desktop\\amd64\\release\\bin\\*.dll" ./Assets/Plugins/
-cp "C:\\Program Files\\Azure Kinect Body Tracking SDK\\sdk\\windows-desktop\\amd64\\release\\bin\\\\*" ./Assets/Plugins/
+cp /c/Program\ Files/Azure\ Kinect\ SDK\ v1.3.0/sdk/windows-desktop/amd64/release/bin/*.dll ./
+cp /c/Program\ Files/Azure\ Kinect\ Body\ Tracking\ SDK/sdk/windows-desktop/amd64/release/bin/* ./
+cp /c/Program\ Files/Azure\ Kinect\ SDK\ v1.3.0/sdk/windows-desktop/amd64/release/bin/*.dll ./Assets/Plugins/
+cp /c/Program\ Files/Azure\ Kinect\ Body\ Tracking\ SDK/sdk/windows-desktop/amd64/release/bin/* ./Assets/Plugins/
