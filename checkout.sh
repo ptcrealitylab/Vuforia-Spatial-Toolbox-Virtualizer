01b43410601f9b81ca56c95d030cedf511aa987a
James Hobin <jhobin@ptc.com>
2020-12-16 11:07:05 -0500

    Add initial checkout.sh

---
+++ checkout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+cp "C:\\Program Files\\Azure Kinect SDK v1.3.0\\sdk\\windows-desktop\\amd64\\release\\bin\\*.dll" ./
+cp "C:\\Program Files\\Azure Kinect Body Tracking SDK\\sdk\\windows-desktop\\amd64\\release\\bin\\\\*" ./
+cp "C:\\Program Files\\Azure Kinect SDK v1.3.0\\sdk\\windows-desktop\\amd64\\release\\bin\\*.dll" ./Assets/Plugins/
+cp "C:\\Program Files\\Azure Kinect Body Tracking SDK\\sdk\\windows-desktop\\amd64\\release\\bin\\\\*" ./Assets/Plugins/
