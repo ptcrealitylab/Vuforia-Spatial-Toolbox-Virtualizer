8caa85cf73b52250d1861f00d5c02b9d1b6a2ed8
James Hobin <jhobin@ptc.com>
2021-05-19 11:23:25 -0400

    Update OPS for new socketio implementation; Enable OPS

--- Assets/AK_RealityZone/Scripts/ObjectPositionSender.cs
+++ Assets/AK_RealityZone/Scripts/ObjectPositionSender.cs
@@ -13,23 +13,17 @@ using Dpoch.SocketIO;
 public class ObjectPositionSender : MonoBehaviour {
     private SocketIO socket;
     private bool connected;
-    public string objectServerUrl = "http://127.0.0.1:8080";
+    public string objectServerUrl = "127.0.0.1:8080";
 
     void Start()
     {
-        // socket = GetComponent(typeof(SocketIOComponent)) as SocketIOComponent;
-        /*
-        SocketOptions options = new SocketOptions();
-        options.AutoConnect = false;
+        socket = new SocketIO("ws://" + objectServerUrl + "/socket.io/?EIO=4&transport=websocket");
 
-        Manager = new SocketManager(new System.Uri(objectServerUrl + "/socket.io/"), options);
-        Manager.Socket.On(SocketIOEventTypes.Connect, OnConnected);
-        Manager.Socket.On(SocketIOEventTypes.Disconnect, OnDisconnected);
+        socket.OnOpen += OnConnected;
+        socket.OnClose += OnDisconnected;
+        socket.OnError += (err) => Debug.Log("Socket Error: " + err);
 
-        Manager.Socket.On(SocketIOEventTypes.Error, (socket, packet, args) => Debug.LogError(string.Format("Error: {0}", args[0].ToString())));
-
-        Manager.Open();
-        */
+        socket.Connect();
     }
 
     void Update()
@@ -39,17 +33,17 @@ public class ObjectPositionSender : MonoBehaviour {
 
     void OnDestroy()
     {
-        // socket.Close();
+        socket.Close();
     }
 
-    private void OnConnected(SocketIOEvent e)
+    private void OnConnected()
     {
         //identify yourself as the station.
         Debug.Log("OPS connected to server");
         connected = true;
     }
 
-    private void OnDisconnected(SocketIOEvent e)
+    private void OnDisconnected()
     {
         connected = false;
         Debug.Log("OPS server disconnected");
@@ -62,14 +56,13 @@ public class ObjectPositionSender : MonoBehaviour {
             return;
         }
 
-        // socket.Emit(skeletons.ToString());
-        // Manager.Socket.Emit("/update/humanPoses", skeletons.ToString());
+        socket.Emit("/update/humanPoses", skeletons.ToString());
         /** The following code would use a fancier API for generic positioning instead
          of just sending raw skeleton data
         foreach (var skeleton in skeletons.Values)
         {
             string topLevelKey = "human" + skeleton["id"].ToString();
-            
+
             foreach (var joint in skeleton["joints"])
             {
                 string objectKey = topLevelKey + "joint" + joint.Key;
@@ -84,7 +77,7 @@ public class ObjectPositionSender : MonoBehaviour {
                 updateMsg["position"]["z"] = z;
                 updateMsg["rotationInRadians"] = 0;
                 updateMsg["editorId"] = "test";
-                Manager.Socket.Emit("/update/object/position", updateMsg.ToString());
+                socket.Emit("/update/object/position", updateMsg.ToString());
             }
         }
         */
